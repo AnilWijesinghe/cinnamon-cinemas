3a7c8ba00a4e542399765deaf8e6a6c616095977
Anil Wijesinghe <anilanjuna@gmail.com>
2021-11-14 12:04:30 +0000

    failed test for random generate no of seats and make generic

--- src/seat/randomPick.test.js
+++ src/seat/randomPick.test.js
@@ -3,7 +3,9 @@ const randomPick = require('./randomPick');
 
 test("No of seats randomly generate; between 1 and 3",()=>{
     //Arrange
+    const minNoSeats = 1;
+    const maxNoSeats = 3;
     //Act
-    const getRandomNoOfSeats = randomPick.getRandomNoOfSeats();
+    const getRandomNoOfSeats = randomPick.getRandomNoOfSeats(minNoSeats,maxNoSeats);
     expect(getRandomNoOfSeats).toBe([1,2,3].includes(getRandomNoOfSeats));
 });
