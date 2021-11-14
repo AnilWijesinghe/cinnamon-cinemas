de7c8fa8eae0222254479a512ab0e7c8eecc1ed7
Anil Wijesinghe <anilanjuna@gmail.com>
2021-11-14 13:32:50 +0000

    failed test; validation for check available seats for customer input

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -3,12 +3,16 @@
  * @param seats
  * @param noOfSeats
  */
-const inputValidation = (seats,noOfSeats)=>{
+const inputValidation = (noOfSeats)=>{
  // validate input no of seats between 1 and 3
  if(noOfSeats>3 || noOfSeats<1) throw new Error('Input should be in between 1 and 3');
+};
+
+const seatsAlreadyFilledValidation = (seats)=>{
  // check have none allocate seats
  const availableSeats = Object.entries(seats).filter(([seat, filled]) => filled===false);
  if(availableSeats.length===0) throw new Error('Seats are already filled in the theatre');
 };
 
-module.exports = {inputValidation};
+
+module.exports = {inputValidation,seatsAlreadyFilledValidation};
--- src/validation/validation.test.js
+++ src/validation/validation.test.js
@@ -5,7 +5,7 @@ test("check input in between 1 and 3",()=>{
     // Arrange
     const noOfSeats = 5;
     // Act and Assert
-    expect(()=>validation.inputValidation(null,noOfSeats)).toThrow('Input should be in between 1 and 3');
+    expect(()=>validation.inputValidation(noOfSeats)).toThrow('Input should be in between 1 and 3');
 });
 
 test("Validate seats are already filled",()=>{
@@ -13,5 +13,14 @@ test("Validate seats are already filled",()=>{
     let seats = {A1:true,A2:true,A3:true,A4:true,A5:true,B1:true,B2:true,B3:true,B4:true,B5:true,C1:true,C2:true,C3:true,C4:true,C5:true};
     const noOfSeats = 2;
     //Act and Assert
-    expect(()=>validation.inputValidation(seats,noOfSeats)).toThrow('Seats are already filled in the theatre')
+    expect(()=>validation.seatsAlreadyFilledValidation(seats,noOfSeats)).toThrow('Seats are already filled in the theatre')
+});
+
+test("Validate; check seats are available ordered input",()=>{
+    //Arrange
+    // true = filled / false = available
+    let seats = {A1:true,A2:true,A3:true,A4:true,A5:true,B1:true,B2:true,B3:true,B4:true,B5:true,C1:true,C2:true,C3:true,C4:true,C5:false};
+    const noOfSeats = 2;
+    //Act and Assert
+    expect(()=>validation.seatsAlreadyFilledValidation(seats,noOfSeats)).toThrow('No seats available for customer input')
 });
