0d950d38ffab4e62b9a08ffa4f36212b559644ae
Anil Wijesinghe <anilanjuna@gmail.com>
2021-11-14 12:00:46 +0000

    failed test; No of seats randomly generate input

---
+++ src/seat/randomPick.js
@@ -0,0 +1,5 @@
+const getRandomNoOfSeats = ()=>{
+
+};
+
+module.exports = {getRandomNoOfSeats};
--- src/seat/randomPick.test.js
+++ src/seat/randomPick.test.js
@@ -1,6 +1,9 @@
 const randomPick = require('./randomPick');
 
 
-test("",()=>{
-
+test("No of seats randomly generate; between 1 and 3",()=>{
+    //Arrange
+    //Act
+    const getRandomNoOfSeats = randomPick.getRandomNoOfSeats();
+    expect(getRandomNoOfSeats).toBe([1,2,3].includes(getRandomNoOfSeats));
 });
