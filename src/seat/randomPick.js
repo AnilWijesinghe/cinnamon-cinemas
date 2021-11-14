aa93e883164973aa01c575e91b306b93623d9488
Anil Wijesinghe <anilanjuna@gmail.com>
2021-11-14 12:15:56 +0000

    passed test and refator code for no of seats random generate

--- src/seat/randomPick.js
+++ src/seat/randomPick.js
@@ -1,5 +1,11 @@
-const getRandomNoOfSeats = ()=>{
-
+/**
+ * randomly generate number for given range
+ * @param maxNoOfSeats
+ * @param minNofSeats
+ * @returns {*}
+ */
+const getRandomNoOfSeats = (maxNoOfSeats,minNofSeats)=>{
+    return Math.floor(Math.random() * (maxNoOfSeats - minNofSeats) + minNofSeats);
 };
 
 module.exports = {getRandomNoOfSeats};
--- src/seat/randomPick.test.js
+++ src/seat/randomPick.test.js
@@ -7,5 +7,5 @@ test("No of seats randomly generate; between 1 and 3",()=>{
     const maxNoSeats = 3;
     //Act
     const getRandomNoOfSeats = randomPick.getRandomNoOfSeats(minNoSeats,maxNoSeats);
-    expect(getRandomNoOfSeats).toBe([1,2,3].includes(getRandomNoOfSeats));
+    expect([1,2,3].includes(getRandomNoOfSeats)).toBeTruthy()
 });
